85149b59714774eca90a3cb0aa8eb4941a6fcf7c
Viplove <viploveprakash@gmail.com>
2018-03-02 02:32:48 -0500

    Add Toogle Feature

---
+++ background.js
@@ -0,0 +1,16 @@
+/*chrome.browserAction.onClicked.addListener(function(tab) {
+   chrome.tabs.executeScript(null, {file: "action.js"});
+});*/
+
+var toggle = false;
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggle = !toggle;
+  if(toggle){
+      chrome.browserAction.setIcon({path: "icons/icon_on_16.png", tabId:tab.id});
+      chrome.tabs.executeScript(tab.id, {file:"js/action.js"});
+  }
+  else{
+    chrome.browserAction.setIcon({path: "icons/icon_16.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {file:"js/no-action.js"});
+  }
+});
--- popup.js
+++
@@ -1,9 +0,0 @@
-function click(e) {
-    chrome.tabs.executeScript(null, {file: "action.js"});
-    window.close();
-}
-                              
-document.addEventListener('DOMContentLoaded', function(){
-    var divAction = document.getElementById('clickMe');
-    divAction.addEventListener('click',click);
-});
