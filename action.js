6237c4b1fe661cfc924446b5a988559ed41d23e2
Viplove <viploveprakash@gmail.com>
2018-03-02 01:35:12 -0500

    Add Icons

--- action.js
+++ action.js
@@ -1,6 +1,7 @@
 // code actions which will be implemented into a given page...
 
 (function() {
+    // Block Element Action
     var blockElementsAction = function (element) {
         element.style.background='none';
         element.style.backgroundColor='#f1f2f3';
@@ -8,14 +9,39 @@
         element.style.color = '#333';
         element.style.border = 'none';
     };
+    // Header Action
     var headingAction = function(element) {
-        element.style.backgroundColor='#555';
+        element.style.background='none';
+        //element.style.fontFamily = "'Comic Sans MS, Comic Sans, cursive'+'!important'";
         element.style.fontFamily = 'Comic Sans MS, Comic Sans, cursive';
-        element.style.color = '#fff';
+        element.style.color = '#333';
     }
-    var divs = document.querySelectorAll('div,table,header,footer,section,aside,p,ul,li,a,span,input,button');
+    // Anchor Action
+    var anchorAction = function(element) {
+        element.style.background='none';
+        element.style.backgroundColor='#f1f2f3';
+        element.style.fontFamily = 'Comic Sans MS, Comic Sans, cursive';
+        element.style.fontWeight = 'bold';
+        element.style.textDecoration = 'underline';
+        element.style.color = '#333';
+    }
+    var imgAction = function(element) {
+        element.src='';
+        element.srcset='';
+        element.style.background='none';
+        element.style.display = 'none';
+    }
+    var divs = document.querySelectorAll('body,div,table,header,footer,section,aside,p,ul,li,a,span,input,button');
     for (i=0; i<divs.length; i++) {
         blockElementsAction(divs[i]);
+    }
+    var anchors = document.querySelectorAll('a');
+    for (i=0; i<anchors.length; i++) {
+        anchorAction(anchors[i]);
+    }
+    var imgs = document.querySelectorAll('img,source,picture,svg');
+    for (i=0; i<imgs.length; i++) {
+        imgAction(imgs[i]);
     }
     var heads = document.querySelectorAll('h1,h2,h3,h4,h5,h6');
     for (i=0; i<divs.length; i++) {
