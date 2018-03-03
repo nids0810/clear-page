844d1792be2d5200c63ed8861351e515d58eef7a
Viplove <viploveprakash@gmail.com>
2018-03-03 18:05:19 -0500

    Added Google Analytics

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
 "use strict";
+var _AnalyticsCode = 'UA-115013413-1';
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
 
 var activeTabs = [];
 chrome.browserAction.onClicked.addListener(function(tab) {
+    _gaq.push(['_trackEvent', tab.id, 'clicked']);
     if(activeTabs.length===0){
         activeTabs.push(tab.title);
         chrome.tabs.executeScript(tab.id, {file:"js/action.js"});
