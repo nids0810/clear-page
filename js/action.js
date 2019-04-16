5463144425488611bc1c8baf10428c055937582a
Viplove Prakash <viploveprakash@gmail.com>
2019-04-16 01:09:49 -0400

    Add Edit & Highlight option

--- js/action.js
+++ js/action.js
@@ -46,59 +46,7 @@
         element.style.display = 'none';
     }
 
-    var createToolOptions = function () {
-        //Create tool options
-        var toolOpt = document.createElement("div");
-        toolOpt.style.backgroundColor = "red";
-        toolOpt.setAttribute("id", "tool-option");
-        document.body.appendChild(toolOpt);
-
-        //Create Edit Button
-        var editBtn = document.createElement("button");
-        editBtn.innerHTML = "Edit";
-        editBtn.setAttribute("id", "edit-btn");
-        //editBtn.onclick = convertToPDF;
-        document.getElementById("tool-option").appendChild(editBtn);
-
-        //Create Highlight Button
-        var highlightBtn = document.createElement("button");
-        highlightBtn.innerHTML = "Highlight";
-        highlightBtn.setAttribute("id", "highlight-btn");
-        //highlightBtn.onclick = convertToPDF;
-        document.getElementById("tool-option").appendChild(highlightBtn);
-
-        //Create Save as PDF Button
-        var pdfBtn = document.createElement("button");
-        pdfBtn.innerHTML = "Save as PDF";
-        pdfBtn.setAttribute("id", "pdf-btn");
-        //pdfBtn.onclick = convertToPDF;
-        document.getElementById("tool-option").appendChild(pdfBtn);
-    }
-
-    var highlightText = function () {
-        alert('highlight');
-
-        /*
-            :: -moz - selection { // Code for Firefox
-            color: red;
-            background: yellow;
-        }
-
-        :: selection {
-            color: red;
-            background: yellow;
-        } */
-    }
-
-    var convertToPDF = function () {
-        alert('worked');
-        var doc = new jsPDF();
-        doc.fromHTML($('#content').html(), 15, 15, {
-            'width': 170
-        });
-        doc.save('sample-file.pdf');
-    }
-
+    //Format the text
     var blocks = document.querySelectorAll('body,div,table,tr,td,header,footer,section,aside,p,ul,li,span,main,article,details,dialog,summary,data,cite,pre,code,form');
     for (var i = 0; i < blocks.length; i++) {
         blockElementsAction(blocks[i]);
@@ -127,6 +75,126 @@
     var programs = document.querySelectorAll('script,noscript,applet,embed,object,param');
     for (var i = 0; i < programs.length; i++) {
         programAction(programs[i]);
+    }
+
+    //Create Tool Option
+    var createToolOptions = function () {
+        //Create tool options
+        var toolOpt = document.createElement("div");
+        toolOpt.style.backgroundColor = "red";
+        toolOpt.setAttribute("id", "tool-option");
+        document.body.appendChild(toolOpt);
+
+        //Create Edit Button
+        var editBtn = document.createElement("button");
+        editBtn.innerHTML = "Edit";
+        editBtn.setAttribute("id", "edit-btn");
+        editBtn.onclick = editWebPage;
+        document.getElementById("tool-option").appendChild(editBtn);
+
+        //Create Highlight Button
+        var highlightBtn = document.createElement("button");
+        highlightBtn.innerHTML = "Highlight";
+        highlightBtn.setAttribute("id", "highlight-btn");
+        highlightBtn.onclick = highlightText;
+        document.getElementById("tool-option").appendChild(highlightBtn);
+
+        //Create Save as PDF Button
+        var pdfBtn = document.createElement("button");
+        pdfBtn.innerHTML = "Save as PDF";
+        pdfBtn.setAttribute("id", "pdf-btn");
+        pdfBtn.onclick = convertToPDF;
+        document.getElementById("tool-option").appendChild(pdfBtn);
+    }
+
+    //Edit Button Function
+    let editMode = false;
+    var editWebPage = function () {
+        console.log('Edit WebPage');
+
+        if (!editMode) {
+            editMode = true;
+            $('#edit-btn').css("background-color", "red");
+            $("body").append("<h1 id='edit-mode'>Edit Mode On!</h1>");
+            //Add a new class "web-edited"
+            $("<style>")
+                .prop("type", "text/css")
+                .html("\
+                    .web-edited {\
+                        color: red;\
+                    }\
+                    .web-deleted {\
+                        color: red;\
+                        display: none;\
+                    }")
+                .appendTo("head");
+
+            // make all links unclickable
+            $("a").each(function () {
+                //console.log(this);
+                //$(this).preventDefault();
+            });
+
+            // Click elements in edit mode
+            $(document).click(function (event) {
+
+                if (event.target.id === 'edit-btn' || event.target.id === 'highlight-btn' || event.target.id === 'pdf-btn' || event.target.id === 'tool-option' || event.target.id === 'edit-mode') {
+                    //Don't delete extension elements
+                    console.log($(event.target).text().trim() + ' cannot be deleted');
+                } else if ($(event.target).hasClass('web-edited')) {
+                    //Don't delete already deleted elements
+                    $(event.target).removeClass("web-edited");
+                } else {
+                    //Delete other deleted elements
+                    console.log($(event.target).text().trim() + ' deleted');
+                    $(event.target).addClass("web-edited");
+                }
+            });
+
+        } else {
+            $('.web-edited').each(function () {
+                //you can use this to access the current item
+                $(this).addClass("web-deleted");
+            });
+            $('#edit-btn').css("background-color", "yellow");
+            $('#edit-mode').remove();
+            editMode = false;
+        }
+    }
+
+    //Highlight Button Function
+    let highlightMode = false;
+    var highlightText = function () {
+        console.log('highlight');
+
+        if (!highlightMode) {
+            highlightMode = true;
+            $('#highlight-btn').css("background-color", "red");
+            $("body").append("<h1 id='highlight-mode'>Highlight Mode On!</h1>");
+
+            $("<style>")
+                .prop("type", "text/css")
+                .html("\
+                    ::-moz - selection {\
+                        color: red;\
+                        background: yellow;\
+                    }\
+                    ::selection {\
+                        color: red;\
+                        background: yellow;\
+                    }")
+                .appendTo("head");
+        } else {
+            $('#highlight-btn').css("background-color", "yellow");
+            $('#highlight-mode').remove();
+            highlightMode = false;
+        }
+    }
+
+    //Save as PDF Button Function
+    var convertToPDF = function () {
+        console.log('Convert to PDF');
+        window.print();
     }
 
     createToolOptions();
--- js/background.js
+++ js/background.js
@@ -3,32 +3,66 @@
 ga('send', 'pageview', '/background.html', 'clicked');
 
 var activeTabs = [];
-chrome.browserAction.onClicked.addListener(function(tab) {
-    if (jQuery) {
-        console.log("Jquery on");
-    } else {
-        console.log("Jquery off");
-    }
-    
-    if(activeTabs.length===0){
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (activeTabs.length === 0) {
         activeTabs.push(tab.title);
         ga('send', 'event', 'Extension', 'Activated', 'One Page', activeTabs.length);
-        chrome.tabs.executeScript(tab.id, {file:"js/action.js"});
-        chrome.browserAction.setIcon({path: "icons/icon_on_16.png", tabId:tab.id});
-    }
-    else {
+        //chrome.tabs.executeScript(tab.id, {file:"js/action.js"});
+        executeScripts(null, [{
+                file: "third-party/jquery.min.js"
+            }, {
+                file: "js/action.js"
+            }
+            //{ file: "helper.js" },
+            //{ code: "transformPage();" }
+        ]);
+        chrome.browserAction.setIcon({
+            path: "icons/icon_on_16.png",
+            tabId: tab.id
+        });
+    } else {
         var tabIndex = activeTabs.indexOf(tab.title);
-        if(tabIndex >= 0){
+        if (tabIndex >= 0) {
             activeTabs = activeTabs.filter(item => item !== tab.title);
             ga('send', 'event', 'Extension', 'Deactivated', 'Pages', activeTabs.length);
-            chrome.tabs.executeScript(tab.id, {file:"js/no-action.js"});
-            chrome.browserAction.setIcon({path: "icons/icon_16.png", tabId:tab.id});
-        }
-        else {
+            chrome.tabs.executeScript(tab.id, {
+                file: "js/no-action.js"
+            });
+            chrome.browserAction.setIcon({
+                path: "icons/icon_16.png",
+                tabId: tab.id
+            });
+        } else {
             activeTabs.push(tab.title);
             ga('send', 'event', 'Extension', 'Activated', 'More Pages', activeTabs.length);
-            chrome.tabs.executeScript(tab.id, {file:"js/action.js"});
-            chrome.browserAction.setIcon({path: "icons/icon_on_16.png", tabId:tab.id});
+            /* chrome.tabs.executeScript(tab.id, {
+                file: "js/action.js"
+            }); */
+            executeScripts(null, [{
+                file: "third-party/jquery.min.js"
+            }, {
+                file: "js/action.js"
+            }]);
+            chrome.browserAction.setIcon({
+                path: "icons/icon_on_16.png",
+                tabId: tab.id
+            });
         }
     }
 });
+
+function executeScripts(tabId, injectDetailsArray) {
+    function createCallback(tabId, injectDetails, innerCallback) {
+        return function () {
+            chrome.tabs.executeScript(tabId, injectDetails, innerCallback);
+        };
+    }
+
+    var callback = null;
+
+    for (var i = injectDetailsArray.length - 1; i >= 0; --i)
+        callback = createCallback(tabId, injectDetailsArray[i], callback);
+
+    if (callback !== null)
+        callback(); // execute outermost function
+}
